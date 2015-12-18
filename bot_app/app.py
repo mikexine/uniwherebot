8ae452fa7c90c54bdd59c1440ca3193aab832d88
Michele Colombo <mikexine@gmail.com>
2015-12-18 21:24:22 +0100

    add time and first testing

--- bot_app/app.py
+++ bot_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import json
+import arrow
 
 app = Flask(__name__)
 
@@ -10,17 +10,21 @@ def index():
 
 
 @app.route('/receive', methods=['POST'])
-def foo():
-    data = json.loads(request.data)
-    return str(data)
-
-
-@app.route('/data')
-def names():
-    data = {
-        "first_names": ["John", "Jacob", "Julie", "Jenny"]
-    }
-    return jsonify(data)
+def reply():
+    # get the message
+    Message = request.get_json(silent=True)
+    print Message
+    # prepare the reply
+    User = Message['from']
+    Text = Message['text']
+    MyTime = arrow.utcnow().format('YYYY-MM-DD HH:mm:ss ZZ')
+    ReplyText = 'Prova: ' + str(Text) + ' ' + str(MyTime)
+    ReplyMessage = {
+                    'to': str(User),
+                    'text': str(ReplyText)
+                    }
+    # return the reply
+    return jsonify(ReplyMessage)
 
 
 if __name__ == '__main__':
